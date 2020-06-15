893c6169d02603c6a33c0c03bf2f2ef918283fe2
Yuhan Zhang(Intern) <yuhan.zhang@envision-digital.com>
2020-06-15 17:07:03 +0800

    Updates

---
+++ precache-manifest.c30207d3aef31b3707dbc7087e6408b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faef3a4df6bc1abeee851c089b34b2a2",
+    "url": "/front-end-react-collection/index.html"
+  },
+  {
+    "revision": "cf1b7722475cc174d9ff",
+    "url": "/front-end-react-collection/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d70605c7f1cca707b921",
+    "url": "/front-end-react-collection/static/js/2.a43af38b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/front-end-react-collection/static/js/2.a43af38b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf1b7722475cc174d9ff",
+    "url": "/front-end-react-collection/static/js/main.a1ea6cd0.chunk.js"
+  },
+  {
+    "revision": "4c0eda688b3e1a0be5a0",
+    "url": "/front-end-react-collection/static/js/runtime-main.5b443a26.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/front-end-react-collection/static/media/logo.5d5d9eef.svg"
+  }
+]);
