5d00c02f6f71827acdc734b8a085add2e601a291
Yuhan Zhang(Intern) <yuhan.zhang@envision-digital.com>
2020-06-15 20:49:11 +0800

    adding less

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import {Button} from 'antd';
 import logo from './logo.svg';
-import 'antd/dist/antd.less';
-import './App.less';
+import 'antd/dist/antd.css';
+import './App.css';
 
 class App extends React.Component {
   render(): React.ReactNode {
     return (
       <div className="App">
         <img src={logo} className="App-logo" alt="logo"/>
-        <h1 className="App-link">Front end Collections</h1>
-        <h3>A JavaScript library for building user interfaces</h3>
+        <h1 className="App-title">Front end Collections</h1>
+        <h3 className="App-title">A JavaScript library for building user interfaces</h3>
         <Button
           type="primary"
           size="large">Get Start</Button>
