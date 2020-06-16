62eed007aaf77b286c14b08eec1a6613ddd49b58
Yuhan Zhang(Intern) <yuhan.zhang@envision-digital.com>
2020-06-16 14:21:26 +0800

    less

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,22 @@ import React from 'react';
 import {Button} from 'antd';
 import logo from './logo.svg';
 import 'antd/dist/antd.css';
-import './App.css';
+import './App.less';
 
 class App extends React.Component {
   render(): React.ReactNode {
     return (
-      <div className="App">
-        <img src={logo} className="App-logo" alt="logo"/>
-        <h1 className="App-title">Front end Collections</h1>
-        <h3 className="App-title">A JavaScript library for building user interfaces</h3>
+      <div className='App'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <h1 className='App-title'>Front end Collections</h1>
+        <p>A JavaScript library for building user interfaces</p>
         <Button
-          type="primary"
-          size="large">Get Start</Button>
+          type='primary'
+          size='large'
+        >Get Start
+        </Button>
       </div>
-    )
+    );
   }
 }
 
